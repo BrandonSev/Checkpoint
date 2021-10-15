8ccdb28a8b47fdad9c1eb19a20bdc2ce9dc3542f
BrandonSev <seveste.brandon@gmail.com>
2021-10-15 10:53:00 +0200

    replace text on click button on presenation block

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,3 +2,11 @@ const avatar = document.querySelector(".presentation img");
 avatar.addEventListener("click", () => {
   avatar.src = "image/avatar-bis.svg";
 });
+
+const button = document.querySelector(".presentation button");
+const name = document.querySelector("#name");
+button.addEventListener("click", () => {
+  const namePrompt = prompt("Enter your name");
+  name.innerHtml = namePrompt;
+  name.style.color = "#fff";
+});
