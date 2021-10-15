5c20d72f485bede5a3760a53ae73c8bae00f009a
BrandonSev <seveste.brandon@gmail.com>
2021-10-15 10:57:35 +0200

    change bg color on submit prompt on description block

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,8 +5,11 @@ avatar.addEventListener("click", () => {
 
 const button = document.querySelector(".presentation button");
 const name = document.querySelector("#name");
+const descriptionBg = document.querySelector(".description");
+console.log(descriptionBg);
 button.addEventListener("click", () => {
   const namePrompt = prompt("Enter your name");
   name.innerHtml = namePrompt;
   name.style.color = "#fff";
+  descriptionBg.style.backgroundColor = "#750ff7";
 });
